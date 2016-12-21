fd109b37be45711d9bd1e76418f9eabcce8795bf
Andrew Adare <andrewadare@gmail.com>
2016-12-21 10:18:46 -0700

    Refactor tuner.jl

--- tuner.jl
+++ tuner.jl
@@ -1,7 +1,6 @@
 #!/usr/bin/env julia
 
 println("Loading dependencies...")
-
 using Plots
 using LibSerialPort
 using JSON
@@ -9,101 +8,83 @@ using JSON
 # Plot backend
 gr(size=(900, 500))
 
-address = length(ARGS) == 1 ? ARGS[1] : "/dev/cu.usbmodem2370891"
-
-# Number of points to display
-n = 500
-
-
-# Values for horizontal (time) axis and data
-t, y1, y2 = zeros(n), zeros(n), zeros(n)
-
-# Use update_counter like a static variable with function scope in C
-let update_counter = 0
-    global readmcu
-function readmcu(sp::SerialPort)
+function readmcu(mcu::SerialPort)
+    datadict = Dict()
     msg = chomp(readuntil(mcu, '\n', 100))
-
-    data = Dict()
     if startswith(msg, "{") && endswith(msg, "}")
         try
-            data = JSON.parse(msg)
+            datadict = JSON.parse(msg)
         catch
             println("skipping $msg")
-            return
         end
-        # println(data)
+    else
+        println(msg) # Pass through non-data messages
     end
+    return datadict
+end
+
+# Manage arrays as FIFO queues
+function bump_queues(datadict, times, setpoints, positions)
 
     for key in ["time", "setpoint", "x"]
-        if !haskey(data, key)
-            # println("missing $key")
-            return
-        end
+        haskey(datadict, key) || return false
     end
 
-    tms = data["time"]
-    setpoint =data["setpoint"]
-    x = data["x"]
-    println("$tms, $setpoint, $x")
-
-    # Manage arrays as FIFO queues
-    push!(t, data["time"]/1000) # ms to seconds
-    push!(y1, data["setpoint"])
-    push!(y2, data["x"])
-    if length(t) > n
-        for v in (t, y1, y2)
-            shift!(v)
-        end
+    push!(times, datadict["time"]/1000) # ms to seconds
+    push!(setpoints, datadict["setpoint"])
+    push!(positions, datadict["x"])
+    for v in (times, setpoints, positions)
+        shift!(v)
     end
-
-    # if mod(tms, 100) == 0
-    if update_counter > 10
-        rtplot(t, y1, y2)
-        update_counter = 0
-    end
-
-    update_counter += 1
-end
+    return true
 end
 
-function rtplot(t, y1, y2)
-    plot(t, [y1, y2],
-         title="Data stream", # Not displaying with GR backend (?)
+function rtplot(times, setpoints, positions)
+    plot(times, [setpoints, positions],
          xaxis="time [s]",
          yaxis=("position [% max]", (-110, 110)), # title, limits
          labels=(["setpoint","position"]), # legend
          line=([1 2], [:steppre :path], ["gray" "crimson"], [1.0 0.5]), # lineweights, linetypes, colors, alphas
-         fill=(0, [0.1 0], "dodgerblue"), # y-origin, alpha, colors
-         )
+         fill=(0, [0.1 0], "dodgerblue")) # y-origin, alpha, colors
+
     # Update the plot window
     gui()
 end
 
-println("Creating plot...")
-rtplot(t,y1,y2)
-
-println("Connecting to MCU...")
-mcu = open(address, 115200)
-println("Ready")
-
-while true
-
-    @async begin
-        a = readline(STDIN)
-        gui()
-        a == "q\n" && quit()
-        write(mcu, "$a")
+function process_mcu_stream(mcu, times, setpoints, positions)
+    datadict = readmcu(mcu)
+    updated = bump_queues(datadict, times, setpoints, positions)
+    if updated && times[end] > 25
+        rtplot(times, setpoints, positions)
     end
-
-    @async begin
-        readmcu(mcu)
-        # @sync begin
-            # @async readmcu(mcu)
-            # @async rtplot(t,y1,y2)
-        # end
-    end
-
-    # Give the queued tasks a chance to run
-    sleep(0.0001)
 end
+
+function main()
+
+    # Number of points to display
+    const N = 500
+    const address = length(ARGS) == 1 ? ARGS[1] : "/dev/cu.usbmodem2370891"
+    times, setpoints, positions = zeros(N), zeros(N), zeros(N)
+
+    println("Opening plot window...")
+    rtplot(times, setpoints, positions)
+
+    println("Connecting to MCU...")
+    mcu = open(address, 115200)
+
+    println("Starting I/O loop.")
+    while true
+        @async begin
+            a = readline(STDIN)
+            a == "q\n" && quit()
+            write(mcu, "$a")
+        end
+        @async begin
+            process_mcu_stream(mcu, times, setpoints, positions)
+        end
+        # Give the queued tasks a chance to run
+        sleep(0.001)
+    end
+end
+
+main()
